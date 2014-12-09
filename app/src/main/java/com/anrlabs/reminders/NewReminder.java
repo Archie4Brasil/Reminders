0bee33c243dddbed95ffee1a8f7c35bdd71abaeb
Archie4Brasil <archie.marques@gmail.com>
2014-12-09 09:36:15 -0500

    implementation of Fragment (time and location) Archie: 12/9/2014 09:35

--- app/src/main/java/com/anrlabs/reminders/NewReminder.java
+++ app/src/main/java/com/anrlabs/reminders/NewReminder.java
@@ -1,7 +1,11 @@
 package com.anrlabs.reminders;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by Archie on 12/8/2014.
@@ -13,4 +17,27 @@ public class NewReminder extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.reminder_new);
     }
+
+    //method to handle fragment selected: time or location (default time)
+    public void selectFrag(View fragSelected)
+    {
+        Fragment fillFrame;
+
+        //sets fragment with view form class
+        if(fragSelected==findViewById(R.id.locationFrag))
+        {
+            fillFrame = new LocationFragment();
+        }
+        else
+        {
+            fillFrame = new TimeFragment();
+        }
+
+        //switch the content of the fragment
+        FragmentManager manager = getFragmentManager();
+        FragmentTransaction fragTransaction = manager.beginTransaction();
+        fragTransaction.replace(R.id.main_frag, fillFrame);
+        fragTransaction.commit();
+    }
+
 }
