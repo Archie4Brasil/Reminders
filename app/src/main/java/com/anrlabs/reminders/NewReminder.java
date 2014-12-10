e9ab5c7cb47c5914ef9d338556313083a37ca1b3
Archie4Brasil <archie.marques@gmail.com>
2014-12-10 15:10:33 -0500

    Implementation of time & date on Database Archie: 12/10/2014 15:10

--- app/src/main/java/com/anrlabs/reminders/NewReminder.java
+++ app/src/main/java/com/anrlabs/reminders/NewReminder.java
@@ -6,8 +6,12 @@ import android.app.FragmentTransaction;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 /**
  * Created by Archie on 12/8/2014.
@@ -15,9 +19,13 @@ import android.widget.EditText;
 public class NewReminder extends Activity{
 
     protected Fragment fillFrame;
-    DatabaseHelper dataCarrier;
-    ContentValues dataFiller;
-    EditText titleCarrier, memoCarrier;
+    protected DatabaseHelper dataCarrier;
+    protected ContentValues dataFiller;
+    protected EditText titleCarrier, memoCarrier;
+    private DatePicker datePicked;
+    private TimePicker timePicked;
+    public static String dateDB, timeDB;
+    private int myHour, myMinute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,13 +40,16 @@ public class NewReminder extends Activity{
         titleCarrier = (EditText) findViewById(R.id.titleBox);
         memoCarrier = (EditText) findViewById(R.id.memoBox);
 
+        datePicked = (DatePicker) findViewById(R.id.dateRemember);
+        timePicked = (TimePicker) findViewById(R.id.timeRemeber);
+
     }
+
+
 
     //method to handle fragment selected: time or location (default time)
     public void selectFrag(View fragSelected)
     {
-
-
         //sets fragment with view form class
         if(fragSelected==findViewById(R.id.locationFrag))
         {
@@ -59,18 +70,33 @@ public class NewReminder extends Activity{
     //return to main without saving entry
     public void cancelSaveData(View view) {
         startActivity(new Intent(this, MainActivity.class));
+        Toast.makeText(this, timeDB, Toast.LENGTH_SHORT);
     }
 
-    //overriding back button to save data
 
+
+    //overriding back button to save data
     @Override
     public void onBackPressed() {
-
-        dataFiller = new ContentValues();
-        dataFiller.put(DatabaseHelper.TITLE,);
-
-        dataCarrier = new DatabaseHelper(this, DatabaseHelper.TABLE, null, 1);
+        savingData();
 
         super.onBackPressed();
     }
+
+    public void savingData()
+    {
+        titleCarrier.setInputType(InputType.TYPE_CLASS_TEXT);
+        memoCarrier.setInputType(InputType.TYPE_CLASS_TEXT);
+
+        dataFiller = new ContentValues();
+        dataFiller.put(DatabaseHelper.TITLE, titleCarrier.getText().toString());
+        dataFiller.put(DatabaseHelper.MESSAGE, memoCarrier.getText().toString());
+        dataFiller.put(DatabaseHelper.TIME, TimeFragment.passTime());
+        dataFiller.put(DatabaseHelper.DATE, TimeFragment.passDate());
+
+        dataCarrier = new DatabaseHelper(this, DatabaseHelper.TABLE, null, 1);
+        dataCarrier.addData(dataFiller);
+    }
+
+
 }
